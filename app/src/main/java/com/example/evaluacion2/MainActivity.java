97c140a19796b9cdabd8dc0b7a03f048c89f26ae
AngelVinet <101446482+AngelVinet@users.noreply.github.com>
2022-05-29 22:10:54 -0400

    Evaluación con comentarios

--- app/src/main/java/com/example/evaluacion2/MainActivity.java
+++ app/src/main/java/com/example/evaluacion2/MainActivity.java
@@ -13,6 +13,7 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Variables generales
     private com.google.android.material.textfield.TextInputEditText edNombre;
     private EditText naFecha;
     private com.google.android.material.textfield.TextInputEditText edTelefono;
@@ -26,13 +27,12 @@ public class MainActivity extends AppCompatActivity {
     String descripcion;
 
     private int ultimoAnio, ultimoMes, ultimoDiaDelMes;
-
-
     final Calendar calendario = Calendar.getInstance();
     int anio = calendario.get(Calendar.YEAR);
     int mes = calendario.get(Calendar.MONTH);
     int diaDelMes = calendario.get(Calendar.DAY_OF_MONTH);
 
+    //Función que obtiene la fecha.
     private DatePickerDialog.OnDateSetListener listenerDeDatePicker = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int anio, int mes, int diaDelMes) {
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    //Función que refresca la fecha actual en el editText correspondiente.
     public void refrescarFecha(){
         String fecha = String.format(Locale.getDefault(), "%02d-%02d-%02d", ultimoAnio, ultimoMes+1, ultimoDiaDelMes);
 
@@ -51,19 +52,21 @@ public class MainActivity extends AppCompatActivity {
         naFecha.setText(fecha);
     }
 
+    //Función que se ejecuta una vez se inicia el layout.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Se inicializa el componente editText para mostrar la fecha.
         naFecha = findViewById(R.id.nacimientoFecha);
-
+        //Se asigna a las variables el año, mes y dia actual
         final Calendar calendario = Calendar.getInstance();
         ultimoAnio = calendario.get(Calendar.YEAR);
         ultimoMes = calendario.get(Calendar.MONTH);
         ultimoDiaDelMes = calendario.get(Calendar.DAY_OF_MONTH);
 
-
+        //Función que al presionar click en el editText naFecha muestra la fecha actual por pantalla.
         naFecha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,18 +75,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Se inicializa los componentes del layout.
         edNombre = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.edNombre);
-        naFecha = (EditText) findViewById(R.id.nacimientoFecha);
         edTelefono = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.edTelefono);
         edEmail = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.edEmail);
         edDescripcion = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.edDescripcion);
 
+        //Se recibe la información del layout mostrar_datos y se asigna a las vaariables correspondientes.
         nombre = getIntent().getStringExtra("nombre");
         fecha = getIntent().getStringExtra("fecha");
         telefono = getIntent().getStringExtra("telefono");
         email = getIntent().getStringExtra("email");
         descripcion = getIntent().getStringExtra("descripcion");
 
+        //Se muestra por pantalla la información obtenida.
         edNombre.setText(nombre);
         naFecha.setText(fecha);
         edTelefono.setText(telefono);
@@ -91,6 +96,7 @@ public class MainActivity extends AppCompatActivity {
         edDescripcion.setText(descripcion);
     }
 
+    //Función que envía la información ingresada hacia el activity mostrar_datos y abre dicho layout.
     public void siguiente(View view){
         Intent intent = new Intent(this, mostrar_datos.class);
         intent.putExtra("nombre", edNombre.getText().toString() );
--- app/src/main/java/com/example/evaluacion2/mostrar_datos.java
+++ app/src/main/java/com/example/evaluacion2/mostrar_datos.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class mostrar_datos extends AppCompatActivity {
 
+    //Variables generales.
     private TextView tvNombre;
     private TextView tvFecha;
     private TextView tvTelefono;
@@ -21,23 +22,27 @@ public class mostrar_datos extends AppCompatActivity {
     String email;
     String descripcion;
 
+    //Función que se ejecuta una vez se inicia el layout.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_datos);
 
+        //Iniciando componentes del layout.
         tvNombre = (TextView) findViewById(R.id.tvNombre);
         tvFecha = (TextView) findViewById(R.id.tvFecha);
         tvTelefono = (TextView) findViewById(R.id.tvTelefono);
         tvEmail = (TextView) findViewById(R.id.tvEmail);
         tvDescripcion = (TextView) findViewById(R.id.tvDescripcion);
 
+        //Se recibe la información del primer layout y se dan dicho valores a las variables correspondientes.
         nombre = getIntent().getStringExtra("nombre");
         fecha = getIntent().getStringExtra("fecha");
         telefono = getIntent().getStringExtra("telefono");
         email = getIntent().getStringExtra("email");
         descripcion = getIntent().getStringExtra("descripcion");
 
+        //Muestra la información recibida en los componentes del layout.
         tvNombre.setText("Nombre Completo: " + nombre);
         tvFecha.setText("Fecha de Nacimiento: " + fecha);
         tvTelefono.setText("Télefono: " + telefono);
@@ -45,6 +50,7 @@ public class mostrar_datos extends AppCompatActivity {
         tvDescripcion.setText("Descripción del contacto: " + descripcion);
     }
 
+    //Función que manda los datos mostrados hacia el layout MainActivity para que puedan ser editados.
     public void editarDatos(View view){
         Intent intent2 = new Intent(this, MainActivity.class);
         intent2.putExtra("nombre", nombre);
